f3a790d59e923e4aa956fd6474c241ecfbdc98c9
Rinsha <rinsha@citrusinformatics.com>
2018-03-06 17:07:54 +0530

    hii

---
+++ src/main/java/com/demo/Sample.java
@@ -0,0 +1,41 @@
+package com.demo;
+
+public class Sample {
+
+	public void add() {
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+		System.out.println("hii");
+
+		System.out.println("hii");
+
+		int a = 0;
+		int result = 20 / a;
+
+	}
+
+	public void calculations() {
+
+	}
+
+}
